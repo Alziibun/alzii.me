40b8d94f8ff51eda251167df13c29ef00ea82f68
Alice S <snapp@null.net>
2023-01-29 10:18:37 -0500

    oops

--- src/server.js
+++ src/server.js
@@ -4,7 +4,10 @@ const express = require('express');
 const app = express()
     .use(vhost('babbit.alzii.me', require('private/config/babbit').app))
     .use(vhost('alzii.me', require('private/config/home').app))
-    .listen(8080);
+
+app.listen(8080, () =>{
+    console.log('Listening on port 8080')
+});
 
 app.get('/', (request, response) => {
     response.render('pages/index')
