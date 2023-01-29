4d45980aa0f710169059908ef233aba8577364f9
Alice S <snapp@null.net>
2023-01-29 10:16:49 -0500

    update relative path

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ const vhost = require('vhost');
 const express = require('express');
 
 const app = express()
-    .use(vhost('babbit.alzii.me', require('./private/config/babbit').app))
-    .use(vhost('alzii.me', require('./private/config/home').app))
+    .use(vhost('babbit.alzii.me', require('private/config/babbit').app))
+    .use(vhost('alzii.me', require('private/config/home').app))
     .listen(8080);
 
 app.get('/', (request, response) => {
